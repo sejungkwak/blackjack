d516312cf842f546f9d5194b3a5732199aba7d90
Sejung Kwak <kwak.sejung@gmail.com>
2022-05-16 11:13:25 +0100

    feat: Create Hand class

--- run.py
+++ run.py
@@ -52,6 +52,30 @@ class Deck:
         return self.deck.pop()
 
 
+class Hand:
+    """Represent a player's hand.
+    """
+
+    def __init__(self):
+        self.cards = []
+        self.value = 0
+        self.aces = 0
+
+    def add_card(self, card):
+        self.card = card
+        self.cards.append(card)
+        self.value += values[card.rank]
+
+        if card.rank == "Ace":
+            self.aces += 1
+
+    def adjust_for_ace(self):
+
+        while self.value > 21 and self.aces:
+            self.value -= 10
+            self.aces -= 1
+
+
 # User Input - Betting
 
 # Game Play
